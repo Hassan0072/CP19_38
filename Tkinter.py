de144aef23d2069f832a390b922cc9dd6bf23e12
Noroz-Usmany <49826366+Noroz-Usmany@users.noreply.github.com>
2019-04-28 21:17:29 +0500

    Add files via upload

--- Tkinter.py
+++ Tkinter.py
@@ -1,10 +1,20 @@
-import tkinter
-from tkinter import messagebox 
+import tkinter  
+import tkinter.filedialog
+ 
 root = tkinter.Tk()
 
 
-def ab():
-	messagebox.showinfo("My Title","My Message")
-
-B = tkinter.Button(root , text ="Hello" , command=ab)
-B.pack()
+def print_path():  
+    f = tkinter.filedialog.askopenfilename(
+        parent=root, initialdir='C:/Tutorial',
+        title='Choose file',
+        filetypes=[('png images', '.png'),
+                   ('gif images', '.gif')]
+        )
+ 
+    print(f)
+ 
+b1 = tkinter.Button(root, text='Print path', command=print_path)  
+b1.pack(fill='x')
+ 
+root.mainloop()  
